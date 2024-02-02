28617328f2d22b61918447ebf334bfd73f17beec
haidyeed <haidyeed@gmail.com>
2024-02-02 19:49:25 +0200

    user authentication

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post("register",[UserController::class,'register']);
+
+Route::post('login', [UserController::class,'login'])->name('login');
+
+Route::group(['middleware' => 'auth:api'], function(){
+
+    Route::get('/logout', [UserController::class, 'logout'])->name('user.logout');
+
 });
